fc41d8513dc2a121e2891313de9e1d8ad59ca495
Kahsolt <kahsolt@qq.com>
2023-08-08 00:28:13 +0800

    add 指鹿为马

--- run.cmd
+++ run.cmd
@@ -16,21 +16,22 @@ REM e.g. 指针指向狗 → 预测出全黑
 python atk.py --point 400,200
 python atk.py --point 400,200 --step 40 --eps 0.1 --alpha 0.01
 
-
 REM 无目标攻击 + 限制修改区域到 边缘
 REM e.g. 指针指向狗 → 预测出全黑
 python atk.py --point 400,200 --lim edge --thresh 0.1
+
+REM 无目标攻击 + 限制修改区域到 目标
+REM e.g. 指针指向 狗, 仅修改 脚 区域
+python atk.py --point 400,200 --lim 400,600
 
 
 REM 有目标攻击
 REM e.g. 指针指向狗 → 预测出盆子
 python atk.py --point 400,200 --point_tgt 250,550
 
-
 REM 有目标攻击 + 限制修改区域到 边缘
 REM e.g. 指针指向 狗 → 预测出 盆子, 仅修改 边缘 区域
 python atk.py --point 400,200 --point_tgt 250,550 --lim edge --thresh 0.1
-
 
 REM 有目标攻击 + 限制修改区域到 目标
 REM e.g. 指针指向 狗 → 预测出 盆子, 仅修改 狗 区域
